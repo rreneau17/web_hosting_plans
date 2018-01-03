54147f642505e990bae80968bc25e81775be844b
Rich Reneau <rich.reneau@gmail.com>
2018-01-02 19:43:48 -0500

    created discount calculation method using a prototype statement

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,36 @@ function Plan(name, price, space, dataTransfer, sitePages, discountMos) {
     this.discountMos = discountMos;
 }
 
+// creates a method for all sevice plans
+Plan.prototype.calcDiscount = function(percentIfDisc) {
+    var bestPrice = 0;
+    var currDate = new Date();
+    var currMo = currDate.getMonth() + 1;
+    for (var i = 0; i < this.discountMos.length; i++) {
+        if (currMo === this.discountMos[i]) {
+            bestPrice = this.price * percentIfDisc;
+            break;
+        }
+        else {
+            bestPrice = this.price;
+        } 
+    }
+    return bestPrice;
+}
+
 var basicPlan = new Plan("Basic", 3.99, 100, 1000, 10, [7,8]);
-var proPlan = new Plan("Professional", 5.99, 500, 5000, 50, [7,8,12]);
+var proPlan = new Plan("Professional", 5.99, 500, 5000, 50, [7,8,12,1]);
 var ultPlan = new Plan("Ultimate", 9.99, 2000, 20000, 500, [7,8]);
 
-console.log("The cost of the " + ultPlan.name + " package is $" + ultPlan.price + " per month." )
+
+function main() {
+    console.log("The cost of the " + ultPlan.name + " package is $" + ultPlan.price + " per month." );
+    if (proPlan.calcDiscount(.85) !== proPlan.price) {
+        console.log("\nYou received a discount for the " + proPlan.name + " package. " + "The monthly rate is $" + proPlan.calcDiscount(.85).toFixed(2));
+    }
+    else {
+        console.log("\nYou monthly rate for the " + proPlan.name + " is $" + proPlan.price);
+    }
+}
+
+main();
